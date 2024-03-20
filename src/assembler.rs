6985b3491649eb2750d4d95226cf1b3462189d8b
hexeaktivitat <hexeaktivitat@gmail.com>
2024-03-20 11:59:33 -0400

    assembler framework

--- src/assembler.rs
+++ src/assembler.rs
@@ -1,1 +1,12 @@
+use miette::{Diagnostic, Result, SourceSpan};
+use thiserror::Error;
+
 struct Assembler {}
+
+impl Assembler {
+    pub fn new() -> Self {
+        Self {}
+    }
+
+    pub fn assemble() {}
+}
